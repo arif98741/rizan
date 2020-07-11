633a2a2f10c33b0222861764bfebe6ae815b804c
Ariful <arif98741@gmail.com>
2020-07-12 02:17:59 +0600

    add some routes

--- routes/admin.php
+++ routes/admin.php
@@ -9,6 +9,12 @@ Route::namespace('Admin')->group(function () {
     Route::delete('restaurant/feature/delete/{id}', 'RestaurantController@featureDelete');
     Route::resource('restaurant_category', 'RestaurantCategoryController')->except('show');
     Route::resource('food', 'FoodController')->except('show');
+
+    Route::get('food/feature', 'FoodController@feature')->name('feature');
+    Route::get('food/feature/add', 'FoodController@featureAdd');
+    Route::post('food/feature/store', 'FoodController@featureStore');
+    Route::delete('food/feature/delete/{id}', 'FoodController@featureDelete');
+
     Route::resource('place', 'PlaceController')->except('show');
     Route::resource('page', 'PageController')->except('show');
     Route::resource('team_member', 'TeamMemberController')->except('show');
