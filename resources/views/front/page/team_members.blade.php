6f05e755e44bff1041f71314f8588aec2e6daf91
Ariful Islam <arif98741@gmail.com>
2020-07-28 22:26:30 +0600

    ref change

--- resources/views/front/page/team_members.blade.php
+++ resources/views/front/page/team_members.blade.php
@@ -12,21 +12,21 @@
                         <div class="col-sm-4">
                             <div class="item-each">
                                 <img class="img-fluid"
-                                     src="{{ asset('uploads/team_member/feature/'.$team_member->feature_photo) }}"
+                                     src="{{ asset('public/uploads/team_member/feature/'.$team_member->feature_photo) }}"
                                      alt="{{ $team_member->name.' - '.url('/') }}">
                                 <div class="title">
                                     <h5 class="name">{{ $team_member->name }}</h5>
                                     <p class="desg">{{ $team_member->designation }}</p>
                                     <div class="social-icon">
                                         <a title="facebook"
-                                           href="https://{{ $team_member->facebook }}?ref{{ url('/') }}">
+                                           href="https://{{ $team_member->facebook }}?ref={{ url('/') }}">
                                             <i class="fab fa-facebook-square"></i>
                                         </a>
                                         <a title="instagram"
-                                           href="https://{{ $team_member->instagram }}?ref{{ url('/') }}">
+                                           href="https://{{ $team_member->instagram }}?ref={{ url('/') }}">
                                             <i class="fab fa-instagram"></i>
                                         </a>
-                                        <a title="twitter" href="https://{{ $team_member->twitter }}?ref{{ url('/') }}">
+                                        <a title="twitter" href="https://{{ $team_member->twitter }}?ref={{ url('/') }}">
                                             <i class="fab fa-twitter-square"></i>
                                         </a>
                                     </div>
