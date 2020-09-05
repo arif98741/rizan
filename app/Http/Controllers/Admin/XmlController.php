3b06603948e6eda752d11e940b773236019ff679
Ariful Islam <arif98741@gmail.com>
2020-09-05 11:15:56 +0600

    web ping done

--- app/Http/Controllers/Admin/XmlController.php
+++ app/Http/Controllers/Admin/XmlController.php
@@ -148,7 +148,6 @@ class XmlController extends Controller
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post));
         $response = curl_exec($ch);
         curl_close($ch); // Close the connection
     }
